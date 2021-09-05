61b7e5f1d61518f4ae81ef1fc0476619420d90cb
kumarash <ashish.kumar@autodesk.com>
2021-09-05 12:58:29 +0530

    Initial Commit for setup

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/Header.css";
+
+function Header() {
+	return (
+		<div>
+			<div className="header-left">
+				<img src="" alt="" />
+				<div className="header-search">
+					{/* SearchIcon */}
+					<input type="text" name="" id="" />
+				</div>
+			</div>
+
+			<div className="header-right"></div>
+		</div>
+	);
+}
+
+export default Header;
